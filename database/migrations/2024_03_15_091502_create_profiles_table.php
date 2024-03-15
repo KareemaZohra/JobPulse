69fb560d0f534b7fbd24c7e2518072b41e505f2c
karima Zohra <karima@nsales.dk>
2024-03-15 15:33:46 +0600

    candidate profile table added

---
+++ database/migrations/2024_03_15_091502_create_profiles_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('nationality');
+            $table->integer('age');
+            $table->string('LinkedinLink')->nullable();
+            $table->string('DegreeName');
+            $table->string('university');
+            $table->string('cgpa');
+            $table->string('passingYear');
+            $table->string('HSC');
+            $table->string('college');
+            $table->string('gpa');
+            $table->string('HSCpassingYear');
+            $table->string('companyName')->nullable();
+            $table->string('Designation')->nullable();
+            $table->date('JoiningDate')->nullable();
+            $table->date('LeavingDate')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->restrictOnDelete()->cascadeOnUpdate();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
